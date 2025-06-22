339eb5e073a9bf253c992ad8ae5f5e4b9c288efe
nzdjb <github@2a1b.com>
2025-06-22 09:00:32 +0000

    fix: Fix issue with default values.

--- terms/models.py
+++ terms/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.utils.crypto import get_random_string
 
 
+def random_secret():
+    """Generate a random 32-character string."""
+    return get_random_string(length=32)
+
+
 class Term(models.Model):
     term = models.CharField(max_length=50)
 
@@ -12,8 +17,8 @@ class Term(models.Model):
 
 class Run(models.Model):
     term = models.ForeignKey(Term, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(default=timezone.now())
-    secret = models.CharField(max_length=32, default=get_random_string(32))
+    start_time = models.DateTimeField(default=timezone.now)
+    secret = models.CharField(max_length=32, default=random_secret)
 
     def __str__(self):
         return str(self.pk)
